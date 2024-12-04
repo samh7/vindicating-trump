97f5c67ae11e0749507c3670b34350fc144258ba
samh7 <samh74490@example.com>
2024-12-04 19:40:49 +0300

    add home main component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import HeroSection from "@/components/HeroSection";
+import HomeMain from "@/components/HomeMain";
 
 export default function Home() {
   return (
     <div className="w-full h-[200vh]">
       <HeroSection />
+      <HomeMain />
     </div>
   );
 }
