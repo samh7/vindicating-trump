cc3341a77beeb510e8593a69b6e86871235dc5ba
samh7 <samh74490@example.com>
2024-12-05 01:23:36 +0300

    add prevent overflow in x axis

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
-
 import HeroSection from "@/components/HeroSection";
 import HomeMain from "@/components/HomeMain";
 
 export default function Home() {
   return (
-    <div className="w-full h-[200vh]">
+    <div className="w-screen overflow-x-hidden">
       <HeroSection />
       <HomeMain />
+      {/* <Footer/> */}
     </div>
   );
 }
