94f9e778d295d50ed56b3b7f5684f4ba7ead4641
samh7 <samh74490@example.com>
2024-12-05 15:12:43 +0300

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
 "use client";
+import FeaturingSection from "@/components/FeaturingSection";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import HomeMain from "@/components/HomeMain";
+import OrderBookSection from "@/components/OrderBookSection";
+import TextSection from "@/components/TextSection";
 
 export default function Home() {
   return (
-    <div className="w-screen overflow-x-hidden">
+    <div
+      className="w-screen overflow-x-hidden text-white
+    bg-gradient-to-b  from-[rgba(0,0,0,.2)]  via-[#060717] to-[#060717]
+    "
+    >
       <HeroSection />
       <HomeMain />
-      {/* <Footer/> */}
+      <FeaturingSection />
+      <OrderBookSection />
+      <TextSection />
+      <Footer />
     </div>
   );
 }
