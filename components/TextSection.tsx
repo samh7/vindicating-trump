8dcf03c68e76ff699d1adfa7abe3b8a9686a212c
samh7 <samh74490@example.com>
2024-12-05 15:14:16 +0300

    move text section to own component

---
+++ components/TextSection.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+export default function TextSection() {
+  return (
+    <div
+      className="flex flex-col relative z-[200] h-auto sm:py-[100px] items-center space-y-4
+     bg-gradient-to-b from-[#0c1421]  to-[#060717]
+    text-white"
+    >
+      <div className="text-center w-[80%]">
+        This film is a journey to discover the real Donald Trump, and to make
+        the case for him as a man and as a leader. It begins with a puzzle. How
+        can a man be so intensely loved and hated? Not since Lincoln have we had
+        such a divisive figure on the national scene. The film also considers a
+        danger that Lincoln warned about—the rise in America of a tyrant like
+        Caesar who would promote lawlessness and subvert the foundations of our
+        constitutional republic. Is Trump such a man?
+      </div>
+      <div className="text-center w-[80%]">
+        The film shows Trump’s meteoric rise to billionaire status and cultural
+        celebrity. Yet even if Trump is a larger than life figure who has the
+        dimensions of a Caesar, he never did anything tyrannical. In fact,
+        Trump’s term in office reveals that the real lawlessness comes from
+        Trump’s adversaries, who will stop at nothing to defeat him. The film
+        spells out their tactics, from an invented accusation of treason to
+        criminal indictments intended to lock him up for life, and even an
+        attempted assassination.
+      </div>
+      <div className="text-center w-[80%]">
+        The story line of the film is to show that Trump’s enemies—the Left and
+        the Democrats—are the real Caesar. They are the ones creating
+        lawlessness at the highest levels of government. They despise Trump
+        because he is the only one who has the power and the will to stop them.
+        The film features interviews with Lara Trump, Trump’s daughter in law
+        and co-chair of the Republican National Committee, and Alina Habba,
+        Trump’s attorney. It also has an in-depth one-on-one interview with
+        Trump himself.
+      </div>
+      <div className="text-center w-[80%]">
+        Throughout the film there are entertaining and powerful re-creations of
+        “war rooms”: a media war-room, a war room at the Democratic campaign
+        headquarters, and a legal war-room where the criminal plots against
+        Trump are hatched. This is Dinesh D’Souza’s most urgent and important
+        film. It offers a fresh and eye-opening perspective on the most
+        intriguing and controversial figure in America today. It will inform,
+        amuse, infuriate, engage and finally motivate Americans of all
+        backgrounds.
+      </div>
+    </div>
+  );
+}
